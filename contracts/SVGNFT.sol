3d6867c1f48b0e402aa39fa4ee9067fee2bfb5eb
pavelgor84 <pavelgor@gmail.com>
2022-02-07 13:37:39 +0400

    svgToImageURI_base64

--- contracts/SVGNFT.sol
+++ contracts/SVGNFT.sol
@@ -1,6 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "@openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "base64-sol/Base64.sol"
 
 contract SVGNFT is ERC721URIStorage {
     uint256 tokenCounter;
@@ -11,5 +12,13 @@ contract SVGNFT is ERC721URIStorage {
 
     function create(string memory svg) public {
         _safeMint(msg.sender, tokenCounter);
+        tokenCounter = tokenCounter + 1;
+        //imageURI
+        //tokenURI
+    }
+
+    function svgToImageURI(string memory svg) {
+        //<svg xmlns="http://www.w3.org/2000/svg" height="210" width="400"> <path d="M150 0 L75 200 L225 200 Z" /></svg>
+        //data:image/svg+xml;base64,<Base65-encoding>
     }
 }
