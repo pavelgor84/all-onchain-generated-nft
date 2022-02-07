791a2aade4f6492e54aedb083b9a6addf7efbd57
pavelgor84 <pavelgor@gmail.com>
2022-02-07 15:18:04 +0400

    imageURI_tokenURI

--- contracts/SVGNFT.sol
+++ contracts/SVGNFT.sol
@@ -10,19 +10,52 @@ contract SVGNFT is ERC721URIStorage {
         tokenCounter = 0;
     }
 
-    function create(string memory svg) public {
+    function create(string memory _svg) public {
         _safeMint(msg.sender, tokenCounter);
         tokenCounter = tokenCounter + 1;
         //imageURI
+        string memory imageURI = svgToImageURI(_svg);
         //tokenURI
+        string memory tokenURI = formatTokenURI(imageURI);
     }
 
-    function svgToImageURI(string memory svg) {
+    function svgToImageURI(string memory _svg)
+        public
+        pure
+        returns (string memory)
+    {
         //<svg xmlns="http://www.w3.org/2000/svg" height="210" width="400"> <path d="M150 0 L75 200 L225 200 Z" /></svg>
         //data:image/svg+xml;base64,<Base65-encoding>
         string memory baseURL = "data:image/svg+xml;base64,";
-        string memory imageURI = Base64.encode(
-            bytes(string(abi.encodePacked(svg)))
+        string memory svgBase64Encoded = Base64.encode(
+            bytes(string(abi.encodePacked(_svg)))
         );
+        string memory imageURI = string(
+            abi.encodePacked(baseURL, svgBase64Encoded)
+        );
+        return imageURI;
+    }
+
+    function formatTokenURI(string memory _imageURI)
+        public
+        pure
+        returns (string memory)
+    {
+        string memory baseURL = "data:application/json;base64,";
+        string memory json = string(
+            abi.encodePacked(
+                '{"name": "SVG NFT", "description": "An NFT based on SVG", "attributes": "", "image": "',
+                _imageURI,
+                '"}'
+            )
+        );
+
+        return
+            string(
+                abi.encodePacked(
+                    baseURL,
+                    Base64.encode(bytes(abi.encodePacked(json)))
+                )
+            );
     }
 }
